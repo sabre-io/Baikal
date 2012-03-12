c79a62443b72506b46566b219e0e7be6359054f7
Jérôme Schneider <mail@jeromeschneider.fr>
2012-03-12 13:20:49 +0100

    Added first implementation of admin web interface

---
+++ CoreVersions/Baikal_0.1/BaikalTools.php
@@ -0,0 +1,46 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 Jérôme Schneider <mail@jeromeschneider.fr>
+*  All rights reserved
+*
+*  http://baikal.codr.fr
+*
+*  This script is part of the Baïkal Server project. The Baïkal
+*  Server project is free software; you can redistribute it
+*  and/or modify it under the terms of the GNU General Public
+*  License as published by the Free Software Foundation; either
+*  version 2 of the License, or (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+class BaikalTools {
+	public static function &db() {
+		return $GLOBALS["pdo"];
+	}
+	
+	public static function getUsers() {
+		
+		$aUsers = array();
+		
+		# Fetching user
+		$stmt = BaikalTools::db()->prepare("SELECT * FROM users");
+		$stmt->execute();
+		while(($user = $stmt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_FIRST)) !== FALSE) {
+			$aUsers[] = $user;
+		}
+		
+		reset($aUsers);
+		return $aUsers;
+	}
+}
