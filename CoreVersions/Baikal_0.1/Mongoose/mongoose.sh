f7861ea1313b98d31fd7b7f92e094f43a7ffaa93
Jérôme Schneider <mail@jeromeschneider.fr>
2012-03-19 23:44:33 +0100

    Added Mongoose on branch mongoose to implement standalone webserver

---
+++ CoreVersions/Baikal_0.1/Mongoose/mongoose.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+	platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+	platform='freebsd'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+	platform='osx'
+fi
+
+PATH_docroot=`cd ../Web && pwd`
+echo "Serving $PATH_docroot"
+
+PATH_this="`pwd`/"
+MONGOOSE_builds=$PATH_this"builds/"
+MONGOOSE_phpcgi=$PATH_this"cgi/php-cgi"
+
+if [[ $platform == 'linux' ]]; then
+	MONGOOSE_bin=$MONGOOSE_builds"linux/mongoose"
+elif [[ $platform == 'freebsd' ]]; then
+	echo "FreeBSD !"
+elif [[ $platform == 'osx' ]]; then
+	MONGOOSE_bin=$MONGOOSE_builds"mac/mongoose"
+fi
+
+`$MONGOOSE_bin -I $MONGOOSE_phpcgi -i index.html,index.php -r $PATH_docroot`
