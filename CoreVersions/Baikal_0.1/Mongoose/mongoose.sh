744256040aa10de181630c899c176b1c546ec770
Jérôme Schneider <mail@jeromeschneider.fr>
2012-03-20 09:04:31 +0100

    Added php-cgi binaries for ubuntux64
    Former-commit-id: 1dbba2058e9b841683194bb60ef96a7afd4e8cdf

--- CoreVersions/Baikal_0.1/Mongoose/mongoose.sh
+++ CoreVersions/Baikal_0.1/Mongoose/mongoose.sh
@@ -14,14 +14,16 @@ echo "Serving $PATH_docroot"
 
 PATH_this="`pwd`/"
 MONGOOSE_builds=$PATH_this"builds/"
-MONGOOSE_phpcgi=$PATH_this"cgi/php-cgi"
+MONGOOSE_cgi=$PATH_this"cgi/"
 
 if [[ $platform == 'linux' ]]; then
 	MONGOOSE_bin=$MONGOOSE_builds"linux/mongoose"
+	MONGOOSE_cgibin=$MONGOOSE_cgi"ubuntux64/php-cgi"
 elif [[ $platform == 'freebsd' ]]; then
 	echo "FreeBSD !"
 elif [[ $platform == 'osx' ]]; then
 	MONGOOSE_bin=$MONGOOSE_builds"mac/mongoose"
+	MONGOOSE_cgibin=$MONGOOSE_cgi"mac/php-cgi"
 fi
 
-`$MONGOOSE_bin -I $MONGOOSE_phpcgi -i index.html,index.php -r $PATH_docroot`
+`$MONGOOSE_bin -I $MONGOOSE_cgibin -i index.html,index.php -r $PATH_docroot`
