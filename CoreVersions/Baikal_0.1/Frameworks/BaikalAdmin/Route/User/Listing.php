55f862454abb43a7d415a20eb5c0a9c2c9f321a6
Jérôme Schneider <mail@jeromeschneider.fr>
2012-03-24 20:39:26 +0100

    Corrected numerous function signatures in Flake and BaikalAdmin
    Former-commit-id: 4933190e6f32902643f8e7bf238c61019d551278

--- CoreVersions/Baikal_0.1/Frameworks/BaikalAdmin/Route/User/Listing.php
+++ CoreVersions/Baikal_0.1/Frameworks/BaikalAdmin/Route/User/Listing.php
@@ -4,7 +4,7 @@ namespace BaikalAdmin\Route\User;
 
 class Listing {
 	
-	public function execute(\Flake\Core\Render\Container &$oRenderContainer) {
+	public static function execute(\Flake\Core\Render\Container &$oRenderContainer) {
 		$oRenderContainer->zone("Payload")->addBlock(new \BaikalAdmin\Controler\User\Listing());
 	}
 }
