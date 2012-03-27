0ec2777af0772f94779a12d61e5932d9d5fcecfa
Jérôme Schneider <mail@jeromeschneider.fr>
2012-03-27 13:57:38 +0200

    Pursuing Web admin implementation
    Former-commit-id: 3e249f5739a02c202f00a80525adc9801af1b432

--- CoreVersions/Baikal_0.1/Frameworks/BaikalAdmin/Model/User.php
+++ CoreVersions/Baikal_0.1/Frameworks/BaikalAdmin/Model/User.php
@@ -3,6 +3,57 @@
 namespace BaikalAdmin\Model;
 
 class User extends \Flake\Core\Model\Db {
-	const DATATABLE = 'users';
-	const PRIMARYKEY = 'username';
+	const DATATABLE = "users";
+	const PRIMARYKEY = "id";
+	
+	protected $oIdentityPrincipal = null;
+	
+	public function initByPrimary($sPrimary) {
+		parent::initByPrimary($sPrimary);
+		
+		# Initializing principals
+		$this->oIdentityPrincipal = $this->getIdentityPrincipal();
+	}
+	
+	public function get($sPropName) {
+		try {
+			# does the property exist on the model object ?
+			$sRes = parent::get($sPropName);
+		} catch(\Exception $e) {
+			# no, it may belong to the oIdentityPrincipal model object
+			$sRes = $this->oIdentityPrincipal->get($sPropName);
+		}
+		
+		return $sRes;
+	}
+	
+	public function set($sPropName, $sPropValue) {
+		try {
+			# does the property exist on the model object ?
+			parent::set($sPropName, $sPropValue);
+		} catch(\Exception $e) {
+			# no, it may belong to the oIdentityPrincipal model object
+			$this->oIdentityPrincipal->set($sPropName, $sPropValue);
+		}
+		
+		return $this;
+	}
+	
+	public function persist() {
+		$this->oIdentityPrincipal->persist();
+	}
+	
+	public function destroy() {
+		# we have to delete user, and all related resources (principals, calendars, calendar events, contact books and contacts)
+		die("TODO: Implement destroy() on " . self::getClass() . "(" . __FILE__ . ":" . __LINE__ . ")");
+	}
+	
+	protected function getIdentityPrincipal() {
+		$oPrincipal = new \BaikalAdmin\Model\Principal("principals/" . $this->get("username"));
+		return $oPrincipal;
+	}
+	
+	public function getMailtoURI() {
+		return "mailto:" . rawurlencode($this->get("displayname") . " <" . $this->get("email") . ">");
+	}
 }
