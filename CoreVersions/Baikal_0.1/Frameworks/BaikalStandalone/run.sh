d782439a82d676779f2f165324081520021037fa
Jérôme Schneider <mail@jeromeschneider.fr>
2012-04-06 08:54:13 +0200

    Added licenses

--- CoreVersions/Baikal_0.1/Frameworks/BaikalStandalone/run.sh
+++ CoreVersions/Baikal_0.1/Frameworks/BaikalStandalone/run.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+#################################################################
+#  Copyright notice
+#
+#  (c) 2012 Jérôme Schneider <mail@jeromeschneider.fr>
+#  All rights reserved
+#
+#  http://baikal.codr.fr
+#
+#  This script is part of the Baïkal Server project. The Baïkal
+#  Server project is free software; you can redistribute it
+#  and/or modify it under the terms of the GNU General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+#
+#  The GNU General Public License can be found at
+#  http://www.gnu.org/copyleft/gpl.html.
+#
+#  This script is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  This copyright notice MUST APPEAR in all copies of the script!
+#################################################################
+
 PATH_SCRIPTFILE=`readlink -f $0`
 PATH_SCRIPTDIR=`dirname $PATH_SCRIPTFILE`"/"
 PATH_ROOT=`dirname $(dirname $(dirname $(dirname $PATH_SCRIPTDIR)))`"/"	# ../../../../
