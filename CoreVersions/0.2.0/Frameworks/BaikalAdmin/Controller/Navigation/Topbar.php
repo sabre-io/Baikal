35f39e38df586083a21212faab2b9123d32da11e
Jérôme Schneider <mail@jeromeschneider.fr>
2012-05-08 23:54:07 +0200

    Working on web admin

--- CoreVersions/0.2.0/Frameworks/BaikalAdmin/Controller/Navigation/Topbar.php
+++ CoreVersions/0.2.0/Frameworks/BaikalAdmin/Controller/Navigation/Topbar.php
@@ -64,31 +64,6 @@ class Topbar extends \Flake\Core\Controller {
 			$sActiveSettingsSystem = "active";
 		}
 		
-<<<<<<< HEAD
-		$sHtml =<<<HTML
-		<div class="navbar navbar-fixed-top">
-			<div class="navbar-inner">
-				<div class="container">
-					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</a>
-					<a class="brand" href="{$sHomeLink}"><img style="vertical-align: text-top; line-height: 20px;" src="res/core/BaikalAdmin/Templates/Page/baikal-text-20.png" /> Web Admin</a>
-					<div class="nav-collapse">
-						<ul class="nav">
-							<li class="{$sActiveHome}"> <a href="{$sHomeLink}">Dashboard</a></li>
-							<li class="{$sActiveUsers}"> <a href="{$sUsersLink}">Users and resources</a></li>
-							<li class="{$sActiveSettingsStandard}"> <a href="{$sSettingsStandardLink}">Settings</a></li>
-							<li class="{$sActiveSettingsSystem}"> <a href="{$sSettingsSystemLink}">System settings</a></li>
-						</ul>
-					</div><!--/.nav-collapse -->
-				</div>
-			</div>
-		</div>
-HTML;
-		return $sHtml;
-=======
 		$oView->setData("activehome", $sActiveHome);
 		$oView->setData("activeusers", $sActiveUsers);
 		$oView->setData("activesettingsstandard", $sActiveSettingsStandard);
@@ -100,6 +75,5 @@ HTML;
 		$oView->setData("logoutlink", $sLogoutLink);
 		
 		return $oView->render();
->>>>>>> 140b415248d6a98d9f3aa37815964b3a4456cfd6
 	}
 }
