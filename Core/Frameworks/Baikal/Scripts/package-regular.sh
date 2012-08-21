b5c9511a27e924dccad2074a05c3e46d47481750
Jérôme Schneider <mail@jeromeschneider.fr>
2012-08-21 10:11:07 +0200

    Packaging scripts have been improved, and now use https://github.com/Kentzo/git-archive-all to archive the project and all submodules.

--- Core/Frameworks/Baikal/Scripts/package-regular.sh
+++ Core/Frameworks/Baikal/Scripts/package-regular.sh
@@ -7,30 +7,26 @@ echo "#     Packaging project for regular distribution"
 echo "#"
 echo "#"
 
-BRANCH="master"
-SRCDIR="../../../../"
-TEMPDIR="/tmp/baikal-regular-`date +%Y-%m-%d-%H-%M-%S`"
+TEMPDATE="`date +%Y-%m-%d-%H-%M-%S`"
+TEMPDIR="/tmp/baikal-regular-$TEMPDATE"
+TEMPARCHIVE="$TEMPDIR/temparchive.tgz"
 
 # Export Project
-mkdir $TEMPDIR
-
-BASEDIR="`dirname $0`"
-PATH_this="`cd $BASEDIR && cd $SRCDIR && pwd`/"
-git archive $BRANCH | tar -x -C $TEMPDIR
+# Requires the git-archive-all script by https://github.com/Kentzo (https://github.com/Kentzo/git-archive-all)
+mkdir $TEMPDIR && \
+git-archive-all --force-submodules $TEMPARCHIVE && \
+cd $TEMPDIR && tar -xzf $TEMPARCHIVE && rm $TEMPARCHIVE && \
 
 # Jump to tempdir
 cd $TEMPDIR && \
 
-# Cleaning git stuff
-rm .gitignore
-
 # Cleaning Scripts
-rm -Rf Core/Scripts
-rm -Rf Core/Frameworks/Baikal/Scripts
+rm -Rf Core/Scripts && \
+rm -Rf Core/Frameworks/Baikal/Scripts && \
 
 # Deploy empty DB
-mkdir -p Specific/db
-cp Core/Resources/Db/SQLite/db.sqlite Specific/db
+mkdir -p Specific/db && \
+cp Core/Resources/Db/SQLite/db.sqlite Specific/db && \
 
 # Displaying result
 echo "#     "$TEMPDIR
