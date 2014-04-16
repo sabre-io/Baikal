d3df29747de5b273fc2c8d3016d546c7d1746cda
Tim Bolender <tim@bolender.info>
2014-04-16 23:11:24 +0200

    Added possibility to edit calendar color
    Former-commit-id: e7ded42c8f556561a6ff072c8e2c2678dd7083f2

--- Core/Frameworks/Baikal/Model/Calendar.php
+++ Core/Frameworks/Baikal/Model/Calendar.php
@@ -162,6 +162,17 @@ class Calendar extends \Flake\Core\Model\Db {
 			)
 		)));
 
+        $oMorpho->add(new \Formal\Element\Text(array(
+            "prop" => "calendarcolor",
+            "label" => "Calendar color",
+            "validation" => "color",
+            "popover" => array(
+                    "title" => "Calendar color",
+                    "content" => "This is the color that will be displayed in your CalDAV client.</br>".
+                    "Must be supplied in format '#RRGGBBAA' with hexadecimal values. This value is optional.",
+            )
+        )));
+
 		$oMorpho->add(new \Formal\Element\Text(array(
 			"prop" => "description",
 			"label" => "Description"
