9b0c737d785f7df312c03f64f92f9c553efa04bf
Jérôme Schneider <contact@netgusto.com>
2014-11-03 14:06:26 +0100

    Cleanup
    Former-commit-id: 4a72bb08a1258efc2a6a013e20fbadba41c1174c

---
+++ web/apps/calclient2/app/app.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+import Resolver from 'ember/resolver';
+import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
+var App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver: Resolver,
+  rootElement: config.rootElement,
+  parameters: config.parameters
+});
+
+loadInitializers(App, config.modulePrefix);
+
+export default App;
