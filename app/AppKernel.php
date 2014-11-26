63cf1d349bfc126ae2da9fc3503ddb0df0a78db6
Jérôme Schneider <contact@netgusto.com>
2014-11-26 17:57:10 +0100

    Appsdeck

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -48,7 +48,7 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            #$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
