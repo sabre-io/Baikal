662d985c1223cdce6fd4cc34f408174fcc1c4953
Net Gusto <contact@netgusto.com>
2014-11-28 14:33:21 +0100

    Fixing #228

--- Core/Frameworks/Baikal/WWWRoot/card.php
+++ Core/Frameworks/Baikal/WWWRoot/card.php
@@ -52,10 +52,18 @@ if(!defined("BAIKAL_CARD_ENABLED") || BAIKAL_CARD_ENABLED !== TRUE) {
 }
 
 # Backends
-if( BAIKAL_DAV_AUTH_TYPE == "Basic" || (array_key_exists('HTTP_USER_AGENT', $_SERVER) && preg_match('/Windows-Phone-WebDAV-Client/i', $_SERVER['HTTP_USER_AGENT'])) )
-    $authBackend = new \Baikal\Core\PDOBasicAuth($GLOBALS["DB"]->getPDO(), BAIKAL_AUTH_REALM);
-else
-    $authBackend = new \Sabre\DAV\Auth\Backend\PDO($GLOBALS["DB"]->getPDO());
+if(
+	BAIKAL_DAV_AUTH_TYPE == "Basic" || (
+		array_key_exists('HTTP_USER_AGENT', $_SERVER) && (
+			preg_match('/Windows-Phone-WebDAV-Client/i', $_SERVER['HTTP_USER_AGENT']) ||
+			preg_match('/MSFT-WP\/8.10.*/i', $_SERVER['HTTP_USER_AGENT'])
+		)
+	)
+) {
+	$authBackend = new \Baikal\Core\PDOBasicAuth($GLOBALS["DB"]->getPDO(), BAIKAL_AUTH_REALM);
+} else {
+	$authBackend = new \Sabre\DAV\Auth\Backend\PDO($GLOBALS["DB"]->getPDO());
+}
 
 $principalBackend = new \Sabre\DAVACL\PrincipalBackend\PDO($GLOBALS["DB"]->getPDO());
 $carddavBackend = new \Sabre\CardDAV\Backend\PDO($GLOBALS["DB"]->getPDO()); 
