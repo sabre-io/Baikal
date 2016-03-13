f54d5c8a95099645ac1b8d36ff97f31aa676cdfd
fabianwolf <fabian--wolf@t-online.de>
2016-03-13 16:48:28 +0100

    fix empty sqlite file
    currently a empty sqlite file  is being created when setting sqlite path
    on non existing file
    
    Fix php warn at install
    
    Turns out Configured version is not defined at first ;)

--- Core/Distrib.php
+++ Core/Distrib.php
@@ -24,5 +24,5 @@
 #  This copyright notice MUST APPEAR in all copies of the script!
 #################################################################
 
-define("BAIKAL_VERSION", "0.4.0");
-define("BAIKAL_HOMEPAGE", "http://baikal-server.com");
+define("BAIKAL_VERSION", "0.5.0");
+define("BAIKAL_HOMEPAGE", "http://sabre.io/baikal/");
