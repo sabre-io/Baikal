5404189daac739b57d24af198bfcde5acd26a101
felixfkrull <github@grupr.de>
2016-08-03 22:58:11 +0200

    create time zone list dynamically make checkboxes app.config-dependent

--- src/Controller/Admin/StandardSettingsController.php
+++ src/Controller/Admin/StandardSettingsController.php
@@ -5,6 +5,7 @@ namespace Baikal\Controller\Admin;
 use Baikal\Controller\Controller;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig_Environment;
+use DateTimeZone;
 
 final class StandardSettingsController extends Controller
 {
@@ -16,6 +17,9 @@ final class StandardSettingsController extends Controller
 
     function indexAction()
     {
-        return $this->render('admin/settings/standard');
+        return $this->render('admin/settings/standard', [
+            'currentTimeZone' => 'Europe/Paris', #must be a settings value
+        	'timeZones' => DateTimeZone::listIdentifiers()
+        ]);
     }
 }
