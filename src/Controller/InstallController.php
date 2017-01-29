d2791064c57c67e508a7c77efdcc6637539d8360
Evert Pot <me@evertpot.com>
2017-01-29 13:07:49 -0500

    Made the first installation step. Verifying if PHP is correctly configured.

--- src/Controller/InstallController.php
+++ src/Controller/InstallController.php
@@ -19,10 +19,53 @@ class InstallController implements ControllerProviderInterface {
 
     }
 
-    function installAction() {
+    function installAction(Application $app) {
 
-        return 'hello darkness, my old friend';
+        $minPhpVersion = '5.5.0';
 
+        $compat = [];
+        $compat[] = [
+            'label' => 'PHP Version >= ' .  $minPhpVersion,
+            'status' =>  version_compare(PHP_VERSION, $minPhpVersion) >= 0 ? 'good' : 'bad',
+            'extraInfo' => 'Your PHP version must at least be ' . $minPhpVersion . '. Try upgrading PHP before running the installer',
+        ];
+        $compat[] = [
+            'label' => 'LibXML version >= 20700',
+            'status' =>  !defined('LIBXML_VERSION') && LIBXML_VERSION >= 20700 ? 'good' : 'bad',
+            'extraInfo' => 'The compiled in libxml version must be higher than 20700. Have ' .  (defined('LIBXML_VERSION')? LIBXML_VERSION : '(none)') . '  installed. Upgrade your operating system\'s packages.',
+        ];
+
+        $extensions =  [
+
+            // sabre/dav main dependencies
+            'dom',
+            'pcre',
+            'spl',
+            'simplexml',
+            'mbstring',
+            'ctype',
+            'date',
+            'iconv',
+            'xmlreader',
+            'xmlwriter',
+
+            // Baikal needs
+            'pdo',
+        ];
+
+        foreach($extensions as $extension) {
+            $compat[]  = [
+                'label' =>  'PHP extension:  ' . $extension,
+                'status' => extension_loaded($extension) ? 'good' : 'bad',
+                'extraInfo' => 'The ' .  $extension . ' PHP extension is required for Baïkal to function.  On Ubuntu/Debian systems you might be able to install this with <code>apt-get install php-' .  $extension . '</code>',
+            ];
+        }
+
+
+
+        $vars = ['compatTable' => $compat];
+
+        return $app['twig']->render('install.html', $vars);
     }
 
 }
