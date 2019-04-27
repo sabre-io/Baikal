ffb5476d0831e71f7c084a3263039f684636ea82
Ben RUBSON <ben.rubson@gmail.com>
2019-04-27 13:12:09 +0200

    Disable notification if BAIKAL_INVITE_FROM not set

--- Core/Frameworks/Baikal/Model/Config/Standard.php
+++ Core/Frameworks/Baikal/Model/Config/Standard.php
@@ -177,7 +177,7 @@ define("BAIKAL_CARD_ENABLED", TRUE);
 # CalDAV ON/OFF switch; default TRUE
 define("BAIKAL_CAL_ENABLED", TRUE);
 
-# CalDAV invite From: mail address
+# CalDAV invite From: mail address (comment or leave blank to disable notifications)
 define("BAIKAL_INVITE_FROM", "noreply@".\$_SERVER["SERVER_NAME"]);
 
 # WebDAV authentication type; default Digest
