a49ceb8c4ca70dc67131794b18c1d05f1e3bcca1
Ben RUBSON <ben.rubson@gmail.com>
2019-04-27 12:46:14 +0200

    Typo

--- Core/Frameworks/Baikal/Model/Config/Standard.php
+++ Core/Frameworks/Baikal/Model/Config/Standard.php
@@ -178,7 +178,7 @@ define("BAIKAL_CARD_ENABLED", TRUE);
 define("BAIKAL_CAL_ENABLED", TRUE);
 
 # CalDAV invite From: mail address
-define("BAIKAL_INVITE_FROM", 'noreply@'.$_SERVER['SERVER_NAME']);
+define("BAIKAL_INVITE_FROM", "noreply@".\$_SERVER["SERVER_NAME"]);
 
 # WebDAV authentication type; default Digest
 define("BAIKAL_DAV_AUTH_TYPE", "Digest");
