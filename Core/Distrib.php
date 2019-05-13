5e848e3b195541a7ce454cbf4e9740b5e3aa724e
ByteHamster <info@bytehamster.com>
2019-05-13 16:25:07 +0200

    Bumped version to 0.5.3

--- Core/Distrib.php
+++ Core/Distrib.php
@@ -24,5 +24,5 @@
 #  This copyright notice MUST APPEAR in all copies of the script!
 #################################################################
 
-define("BAIKAL_VERSION", "0.5.2");
+define("BAIKAL_VERSION", "0.5.3");
 define("BAIKAL_HOMEPAGE", "http://sabre.io/baikal/");
