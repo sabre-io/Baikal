8476feb2db5f2b0d82708346a5113a1b17fcb141
Phil Davis <phil@jankaritech.com>
2022-10-09 03:32:39 +0545

    Fix typo in comment

--- Core/Frameworks/BaikalAdmin/WWWRoot/install/index.php
+++ Core/Frameworks/BaikalAdmin/WWWRoot/install/index.php
@@ -53,7 +53,7 @@ if (!file_exists(PROJECT_PATH_ROOT . 'vendor/')) {
 
 require PROJECT_PATH_ROOT . "vendor/autoload.php";
 
-# Bootstraping Flake
+# Bootstrapping Flake
 \Flake\Framework::bootstrap();
 
 # Bootstrap BaikalAdmin
