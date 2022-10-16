0bfa4a849429ec8a2699ad27d4cb71009ea0eacd
El-Virus <eloimc4@gmail.com>
2022-10-16 16:32:25 +0200

    Moved Structs folder to correct location.

---
+++ Core/Frameworks/Baikal/Model/Structs/LDAPConfig.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Baikal\Model\Structs;
+
+/**
+ * Struct that holds the Configuration parameters for LDAP authentication.
+ *
+ * @author El-Virus <elvirus@ilsrv.com>
+ * @license http://sabre.io/license/ Modified BSD License
+ */
+class LDAPConfig {
+    public $ldap_mode;
+    public $ldap_uri;
+    public $ldap_bind_dn;
+    public $ldap_bind_password;
+    public $ldap_dn;
+    public $ldap_cn;
+    public $ldap_mail;
+    public $ldap_search_base;
+    public $ldap_search_attribute;
+    public $ldap_search_filter;
+    public $ldap_group;
+}
