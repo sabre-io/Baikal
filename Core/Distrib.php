3ca9b4fc53350b82f595213068a5c02c73e3bd04
ByteHamster <ByteHamster@users.noreply.github.com>
2023-11-15 21:15:17 +0100

    Bump version to 0.9.4 (#1217)

--- Core/Distrib.php
+++ Core/Distrib.php
@@ -25,5 +25,5 @@
 #  This copyright notice MUST APPEAR in all copies of the script!
 #################################################################
 
-define("BAIKAL_VERSION", "0.9.3");
+define("BAIKAL_VERSION", "0.9.4");
 define("BAIKAL_HOMEPAGE", "https://sabre.io/baikal/");
