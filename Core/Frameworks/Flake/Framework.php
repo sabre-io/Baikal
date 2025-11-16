c2651e3adc657d0b4479095693348d6b8f77ae80
mestrode <35630715+mestrode@users.noreply.github.com>
2025-11-16 09:49:39 +0100

    Optional CA Certificate for MySQL connection via TLS (#1348)

--- Core/Frameworks/Flake/Framework.php
+++ Core/Frameworks/Flake/Framework.php
@@ -331,7 +331,8 @@ class Framework extends \Flake\Core\Framework {
                 $config['database']['mysql_host'],
                 $config['database']['mysql_dbname'],
                 $config['database']['mysql_username'],
-                $config['database']['mysql_password']
+                $config['database']['mysql_password'],
+                $config['database']['mysql_ca_cert']
             );
 
             # We now setup t6he connexion to use UTF8
