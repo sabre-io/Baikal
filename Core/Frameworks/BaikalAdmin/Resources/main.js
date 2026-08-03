8879886879ff8e70975f9e5c1a283d54bac971cf
Cthulhux <github@tuxproject.de>
2026-08-03 09:19:43 +0200

    Update JQuery to 4.0.0 (#1420)

--- Core/Frameworks/BaikalAdmin/Resources/main.js
+++ Core/Frameworks/BaikalAdmin/Resources/main.js
@@ -8,14 +8,25 @@ $(document).ready(function() {
     });
 });
 
-function copyToClipboard(el) {
-    var range = document.createRange();
-    range.selectNodeContents(el);
-    var sel = window.getSelection();
-    sel.removeAllRanges();
-    sel.addRange(range);
-    document.execCommand("copy");
-    sel.removeAllRanges();
-    $(el).css({backgroundColor:"#75c753"});
-    $(el).animate({backgroundColor:"transparent"}, 1500);
+async function copyToClipboard(el) {
+    const type = "text/plain";
+    const clipboardItemData = {
+        [type]: $(el).text(),
+    };
+    const clipboardItem = new ClipboardItem(clipboardItemData);
+    await navigator.clipboard.write([clipboardItem]);
+    flash(el);
+}
+
+function flash(el) {
+    el.style.transition = "none";
+    el.style.backgroundColor = "#75c753";
+    void el.offsetWidth;
+    el.style.transition = "background-color 1500ms ease";
+    el.style.backgroundColor = "transparent";
+    el.addEventListener("transitionend", function handler() {
+        el.style.transition = "";
+        el.style.backgroundColor = "";
+        el.removeEventListener("transitionend", handler);
+    });
 }
