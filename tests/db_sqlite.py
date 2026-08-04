ba8ef9e49a2e0e044cc4561c60b2cf02dfa76cd4
Hans-Peter Lehmann <ByteHamster@users.noreply.github.com>
2026-08-04 10:58:35 +0200

    Run tests on dist version as well (#1431)

--- tests/db_sqlite.py
+++ tests/db_sqlite.py
@@ -1,8 +1,8 @@
 import mechanicalsoup, os
-from test_helpers import assert_installed, assert_dashboard, assert_upgrade, setup_admin_password
+from test_helpers import ROOT_DIR, assert_installed, assert_dashboard, assert_upgrade, setup_admin_password
 
 def setup():
-    db_path = "Specific/db/db.sqlite"
+    db_path = os.path.join(ROOT_DIR, "Specific/db/db.sqlite")
     if os.path.exists(db_path):
         os.remove(db_path)
 
@@ -36,10 +36,10 @@ system:
     base_uri: ''
     invite_from: noreply@localhost
 database:
-    sqlite_file: {os.getcwd()}/Specific/db/db.sqlite
+    sqlite_file: {os.path.abspath(os.path.join(ROOT_DIR, "Specific/db/db.sqlite"))}
     backend: sqlite
     encryption_key: 89dc0abf3bf079f11df5f009a1b41fe1
 """
-    with open("config/baikal.yaml", "w", encoding="utf-8") as f:
+    with open(os.path.join(ROOT_DIR, "config/baikal.yaml"), "w", encoding="utf-8") as f:
         f.write(config)
     assert_upgrade(browser)
